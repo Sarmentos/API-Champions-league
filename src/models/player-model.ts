488a80892d4e2283bf8027b830d91736cbd8d652
lucas silva sarmento <lucassilvasarmentos3@gmail.com>
2025-07-09 12:34:15 -0300

    feat: implementa endpoint de jogadores

--- src/models/player-model.ts
+++ src/models/player-model.ts
@@ -1,4 +1,10 @@
+import { Statistics } from "./statistics-model";
+
 export interface PlayerModel {
   id: number;
   name: string;
+  club: string;
+  nationality: string;
+  position: string;
+  statistics: Statistics;
 }
